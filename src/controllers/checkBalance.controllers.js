8c286b73cc6142717d4e1c1fc8db70b287c678b1
vergaraldvm <leonardo11vergara11@gmail.com>
2022-11-24 17:07:52 -0500

    askdas

--- src/controllers/checkBalance.controllers.js
+++ src/controllers/checkBalance.controllers.js
@@ -12,6 +12,10 @@ export default async function (req, res) {
         return res.status(500).json({ err })
     }
 
+    console.log('cardFound.owner', cardFound.owner);
+    console.log('card.owner', card.owner);
+    console.log('cardFound.card_number', cardFound.card_type_id);
+    console.log('card.card_number', card.card_type_id);
     if (cardFound.owner !== card.owner || cardFound.card_type_id !== card.card_type_id) {
         return res.status(400).json({ message: 'Error: tarjetas no coinciden' });
     }
